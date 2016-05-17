c18e0953406a174dc7e753761613124ba89e24b1
Ibrar Azeem <ibrar@flikkable.com>
2016-05-17 17:59:53 +0500

    Mongodb adjust queries to check permissions from mongodb collections

--- src/Bican/Roles/Traits/HasRoleAndPermission.php
+++ src/Bican/Roles/Traits/HasRoleAndPermission.php
@@ -164,10 +164,9 @@ trait HasRoleAndPermission
             throw new InvalidArgumentException('[roles.models.permission] must be an instance of \Illuminate\Database\Eloquent\Model');
         }
 
-        return $permissionModel::select(['permissions.*', 'permission_role.created_at as pivot_created_at', 'permission_role.updated_at as pivot_updated_at'])
-                ->join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')->join('roles', 'roles.id', '=', 'permission_role.role_id')
-                ->whereIn('roles.id', $this->getRoles()->lists('id')->toArray()) ->orWhere('roles.level', '<', $this->level())
-                ->groupBy(['permissions.id', 'pivot_created_at', 'pivot_updated_at']);
+        // if want to level base permission checking add this    ->orWhere('roles.level', '<', $this->level()
+        return $permissionModel::whereIn('role_ids',$this->getRoles()->lists('id')->toArray());
+        
     }
 
     /**
